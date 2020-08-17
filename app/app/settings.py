23a28f1e84e269eb4132ba08cc06788b8b52385c
robertbwana <bwanarm@gmail.com>
2020-08-17 15:24:07 +0200

    added image field to recipe model

--- app/app/settings.py
+++ app/app/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
+
 
 # User settings
 AUTH_USER_MODEL = 'core.User'
